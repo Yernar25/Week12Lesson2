f600f15e24c335feb48e00ab69ab2c1154eaa485
Yernar <y.dyussenbekov@gmail.com>
2023-10-17 12:58:02 +0600

    1-st project

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.example.myapplication.databinding.ActivitymainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var  binding: ActivitymainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivitymainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.textView.text = "Hi!"
+        binding.textView.setOnClickListener{
+            Toast.makeText(this, "11111", Toast.LENGTH_SHORT).show()
+        }
+    }
+}
